e0ee4fbeaa83605333cc4e861225b52190c40387
HiPeach <hi@hipeach.eu.org>
2024-08-13 18:12:57 +0800

    🐞 fix: 修复next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
-
-module.exports = {
+export default {
+  nextConfig,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
